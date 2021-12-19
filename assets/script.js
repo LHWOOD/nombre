c93ab774d0c5e4598218e59b453e75c4297b96d1
LHWOOD <hutsonwood@gmail.com>
2021-12-19 14:45:16 -0700

    created buttons and color changes per gender

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,57 @@
-const yo = "hey there";
+const button1 = document.querySelector("btn1");
+const button2 = document.querySelector("btn2");
+const space = document.querySelector("here");
 
-console.log(yo);
+const handleBoy = function (event) {
+  event.preventDefault();
+  getBoy();
+};
+const handleGirl = function (event) {
+  event.preventDefault();
+  getGirl();
+};
+
+function getBoy() {
+  let requestUrl = "";
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      button1.setAttribute("style", "display: none");
+      button2.setAttribute("style", "display: none");
+
+      const verse = document.createElement("p");
+
+      verse.textContent = data.reference + '"' + data.text + '"';
+
+      here.appendChild(verse);
+    });
+}
+
+function getGirl() {
+  let requestUrl = "";
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      button1.setAttribute("style", "display: none");
+      button2.setAttribute("style", "display: none");
+
+      const verse = document.createElement("p");
+
+      verse.textContent = data.reference + '"' + data.text + '"';
+
+      here.appendChild(verse);
+    });
+}
+
+getBoy.addEventListener("click", handleBoy);
+getGirl.addEventListener("click", handleGirl);
